743eb010a4e4bfcbff72cff39fbf731e15fb4fbd
Bjorn Basar <ian0583@gmail.com>
2018-07-12 16:12:51 +1200

    test

---
+++ post.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+$type = $_REQUEST['type'];
+$number = $_REQUEST['value'];
+
+switch ($type)
+{
+	case 'oddsum':
+		$numbers = str_split($number);
+		for($i = 0; $i < count($numbers); $i=$i+2)
+		{
+			$total += $numbers[$i];
+		}
+		break;
+	case 'evensum':
+		$numbers = str_split($number);
+		for($i = 1; $i < count($numbers); $i=$i+2)
+		{
+			$total += $numbers[$i];
+		}
+		break;
+	case 'allsum':
+		$total = ($number * ($number + 1))/2;
+		break;
+}
+
+echo json_encode($total);
